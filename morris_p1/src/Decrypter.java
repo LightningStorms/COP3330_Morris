479d648b5c92c59967fb58dce568c914a88b5c17
Lauren <seaturtlestudent@gmail.com>
2020-09-09 23:21:31 -0400

    Create functional enctryptor/decryptor program

--- morris_p1/src/Decrypter.java
+++ morris_p1/src/Decrypter.java
@@ -2,6 +2,45 @@ public class Decrypter {
     private String test;
 
     public String decrypt(String test){
+        String decrypted = "";
 
+        //add 7
+        decrypted = minus7(test);
+
+        //swap digits
+        decrypted = swap(encrypted, 0);
+        decrypted = swap(encrypted, 1);
+
+        //decrypted now holds the final string...
+
+    }
+
+    //subtracts 7 to each digit
+    public String minus7(String test){
+        int i;
+        String minus7 = "";
+
+        for (i=0; i<4; i++)
+            minus7.charAt[i]= test.charAt[i] + 7;
+
+        return minus7;
+    }
+
+    //can use the same swap as in the encryptor because the numbers are in the same locations
+
+    //swap first and third digits or second and fourth digits based on 'a'
+    //if a is 0, swaps 1 & 3
+    //if a is 1 swaps 2 & 4
+    public String swap(String test, int a){
+        String temp = "";
+
+        /* temp holds the value of 3rd digit so first can be swapped then puts the third
+            digit back in the first slot.
+         */
+        temp.charAt[0]= test.charAt[2+a];
+        test.charAt[2+a]= test.charAt[0+a];
+        test.charAt[0+a]= temp.charAt[0];
+
+        return test;
     }
 }
--- morris_p1/src/Encrypter.java
+++ morris_p1/src/Encrypter.java
@@ -2,6 +2,43 @@ public class Encrypter {
     private String test;
 
     public String encrypt(String test){
-        
+        String encrypted = "";
+
+        //add 7
+        encrypted = add7(test);
+
+        //swap digits
+        encrypted = swap(encrypted, 0);
+        encrypted = swap(encrypted, 1);
+
+        //encrypted now holds the final string...
+
+    }
+
+    // adds 7 to each digit
+    public String add7(String test){
+        int i;
+        String add7 = "";
+
+        for (i=0; i<4; i++)
+            add7.charAt[i]= test.charAt[i] + 7;
+
+        return add7;
+    }
+
+    //swap first and third digits or second and fourth digits based on 'a'
+    //if a is 0, swaps 1 & 3
+    //if a is 1 swaps 2 & 4
+    public String swap(String test, int a){
+        String temp = "";
+
+        /* temp holds the value of 3rd digit so first can be swapped then puts the third
+            digit back in the first slot.
+         */
+        temp.charAt[0]= test.charAt[2+a];
+        test.charAt[2+a]= test.charAt[0+a];
+        test.charAt[0+a]= temp.charAt[0];
+
+        return test;
     }
 }
